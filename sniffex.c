5986e28ab1b09b46c7870a8d0b9a2c0122a916b3
aiqqia <aiqqia.ag@gmail.com>
2021-12-01 04:55:09 +0530

    Added final files

--- sniffex.c
+++ sniffex.c
@@ -1,23 +1,13 @@
 #include <pcap.h>
-
 #include <stdio.h>
-
 #include <string.h>
-
 #include <stdlib.h>
-
 #include <ctype.h>
-
 #include <errno.h>
-
 #include <sys/types.h>
-
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-
 #include <arpa/inet.h>
-
 #include <assert.h>
 
 #define APP_NAME "sniffex"
